c282a72c625615ca0fdb634dc20d73e640b9908d
safafa <safaaballagh97@gmail.com>
2021-06-08 13:11:51 +0100

    Add new form and implement create

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,11 +1,23 @@
 class PostsController < ApplicationController
   before_action :authenticate_user!, except: [:index]
   def new
+    @post = Post.new
   end
 
   def create
+    @post = Post.new(post_params)
+    if @post.save
+      redirect_to root_path
+    else
+      render :new
+    end
   end
 
   def index
   end
+
+  private 
+   def post_params 
+    params.require(:post).permit(:title, :body).merge(user: current_user)
+   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 Rails.application.routes.draw do
-  get 'posts/new'
-  get 'posts/create'
-  get 'posts/index'
+  resources :posts
   devise_for :users
+  root 'posts#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
